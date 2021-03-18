05827859144d40c39907f73d6e5bd7766d9532d9
syh <dbsgur6896@naver.com>
2021-03-18 14:39:50 +0900

    영화 검색 api

--- src/main/java/project/movie/api/MovieApiClient.java
+++ src/main/java/project/movie/api/MovieApiClient.java
@@ -1,13 +1,16 @@
 package project.movie.api;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 
 @Configuration
 public class MovieApiClient {
 
-    private final String clientId = "kmJcoKSSgomqKQx5z6zJ";
-    private final String clientSecret = "D1ouh5MxCy";
+    private String clientId = "[ X-Naver-Client-Id ]";
+    private String clientSecret = "[ X-Naver-Client-Secret ]";
 
     public HttpHeaders getHeaders() {
         HttpHeaders headers = new HttpHeaders();
--- src/main/java/project/movie/controller/MovieController.java
+++ src/main/java/project/movie/controller/MovieController.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import project.movie.controller.dto.RequestMovie;
 import project.movie.service.MovieService;
@@ -14,21 +13,19 @@ import java.util.List;
 
 @Controller
 @Slf4j
-@RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class MovieController {
 
     private final MovieService movieService;
 
-    @GetMapping
-    public String home() {
-        return "movie";
-    }
-
     @GetMapping("/movies")
-    public String findMovie(@RequestParam("query") String query, Model model) {
-        List<RequestMovie> movies = movieService.sort(movieService.search(query));
-        model.addAttribute("movies", movies);
-        return "search";
+    public String findMovie(@RequestParam String query, Model model) {
+        try {
+            List<RequestMovie> movies = movieService.sort(movieService.search(query));
+            model.addAttribute("movies", movies);
+            return "search";
+        } catch (Exception e) {
+            return "redirect:/";
+        }
     }
 }
