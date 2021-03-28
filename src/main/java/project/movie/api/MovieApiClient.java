fe87afb97ac3c3a4f17e09082290df4d60eff609
syh <dbsgur6896@naver.com>
2021-03-28 23:15:43 +0900

    gitignore 설정

--- src/main/java/project/movie/api/MovieApiClient.java
+++ src/main/java/project/movie/api/MovieApiClient.java
@@ -1,18 +1,21 @@
 package project.movie.api;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 
+
 @Configuration
+@ConfigurationProperties(prefix = "naver.openapi")
 public class MovieApiClient {
 
-    private String clientId = "kmJcoKSSgomqKQx5z6zJ";
-    private String clientSecret = "D1ouh5MxCy";
+    private String clientId;
+    private String clientSecret;
 
     public HttpHeaders getHeaders() {
         HttpHeaders headers = new HttpHeaders();
-        headers.add("X-Naver-Client-Id", clientId);
-        headers.add("X-Naver-Client-Secret", clientSecret);
+        headers.add("X-Naver-Client-Id", this.clientId);
+        headers.add("X-Naver-Client-Secret", this.clientSecret);
         return headers;
     }
 }
